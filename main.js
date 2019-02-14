c5da058e09945f4b64485de1a5c514f5fd865630
n.orobenko <nikoro99@gmail.com>
2019-02-14 21:46:59 +0300

    Started making layout #4

---
+++ main.js
@@ -0,0 +1,32 @@
+let listOfTodos = [
+    {
+        title: "FUCK EVERYONE WHO WILL READ THIS!!!",
+        createdAt: new Date().getTime(),
+        status: 0
+    },
+    {
+        title: "Take out the trash",
+        createdAt: new Date().getTime(),
+        status: 0
+    },
+    {
+        title: "Buy bread",
+        createdAt: new Date().getTime(),
+        status: 0
+    },
+    {
+        title: "Teach penguins to fly",
+        createdAt: new Date().getTime(),
+        status: 0
+    }
+];
+
+let objList = () => {
+    return listOfTodos;
+};
+
+function renderApp(){
+
+}
+
+console.log(objList());

c79e2d668761793b9c81f1e7d29895c63b4fe888
n.orobenko <nikoro99@gmail.com>
2019-02-15 21:54:14 +0300

    Start making todo logic.

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
-let listOfTodos = [
-    {
-        title: "FUCK EVERYONE WHO WILL READ THIS!!!",
-        createdAt: new Date().getTime(),
-        status: 0
-    },
-    {
-        title: "Take out the trash",
-        createdAt: new Date().getTime(),
-        status: 0
-    },
-    {
-        title: "Buy bread",
-        createdAt: new Date().getTime(),
-        status: 0
-    },
-    {
-        title: "Teach penguins to fly",
-        createdAt: new Date().getTime(),
-        status: 0
+$(function() {
+    let listOfTodos = [
+        {
+            title: "FUCK EVERYONE WHO WILL READ THIS!!!",
+            createdAt: new Date().getTime(),
+            status: 0
+        },
+        {
+            title: "Take out the trash",
+            createdAt: new Date().getTime(),
+            status: 0
+        },
+        {
+            title: "Buy bread",
+            createdAt: new Date().getTime(),
+            status: 0
+        },
+        {
+            title: "Teach penguins to fly",
+            createdAt: new Date().getTime(),
+            status: 0
+        }
+    ];
+
+    let objList = () => {
+        return listOfTodos;
+    };
+
+    function takeInputData(){
+        let tmp = {
+            title: $("#newTodo").val(),
+            createdAt: new Date().getTime(),
+            status: 0
+        };
+        listOfTodos.push(tmp);
     }
-];
 
-let objList = () => {
-    return listOfTodos;
-};
+    function pushDataOnPage(todoList) {
+        $.each(todoList, (key, value) => {
+            $("#sortable").prepend(`<li class='ui-state-default'>
+    <div class='checkbox'>
+        <label>
+            <input type="checkbox" value="" />${value.text}
+        </label>
+    </div>
+</li>`)
+        })
+    }
 
-function renderApp(){
+    $("#newTodo").onkeypress(function (event) {
+        if (event.which === '13'){
 
-}
+        }
+    })
+});
+
+
 
 console.log(objList());

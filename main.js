744dd32a9e110be9faabd7f5f088ddd50612f66c
n.orobenko <nikoro99@gmail.com>
2019-02-21 21:53:55 +0300

    different things

--- main.js
+++ main.js
@@ -2,38 +2,49 @@ $(document).ready(function() {
     let todosList = [
         {
             title: "FUCK EVERYONE WHO WILL READ THIS!!!",
-            createdAt: new Date().getTime(),
+            createdAt: new Date(),
             status: 0
         },
         {
             title: "Take out the trash",
-            createdAt: new Date().getTime(),
+            createdAt: new Date(),
             status: 0
         },
         {
             title: "Buy bread",
-            createdAt: new Date().getTime(),
-            status: 0
+            createdAt: new Date(),
+            status: 1
         },
         {
             title: "Teach penguins to fly",
-            createdAt: new Date().getTime(),
-            status: 0
+            createdAt: new Date(),
+            status: 2
         }
     ];
+    /*about status
+    * 0 - new
+    * 1 - in process
+    * 2 - done*/
+
+    //console.log(todosList[0].createdAt);
 
     let $newTodo = $("#newTodo");
     let $sortable = $("#sortable");
     let $todoCount = $("#count-todos");
+    let $fireImage = $("#fire");
+    let $triangleImage = $("#triangle");
+    let $smileImage = $("#smile");
 
     $('.selectpicker').selectpicker();
 
     function takeInputData(){
+        var date = moment();
         var tmpObj = {
             title: $newTodo.val(),
-            createdAt: new Date().getTime(),
+            createdAt: date.format("DD-MM-YYYY"),
             status: 0
         };
+        console.log(tmpObj.createdAt);
         return tmpObj;
     }
 
@@ -61,14 +72,20 @@ $(document).ready(function() {
 
     todoRenderList(todosList);
 
-    $newTodo.on('keypress',event => {
+    $newTodo.on('keypress', event => {
         let keyCode = event.keyCode || event.which;
-        if (keyCode == '13'){
+        if (keyCode == '13') {
             let newTodoItem = takeInputData();
             todosList.push(newTodoItem);
             todoCount(todosList);
             todoRenderItem(newTodoItem);
             $newTodo.val('');
         }
+    });
+
+    $fireImage.on('click', event => {
+        $(event.currentTarget).css('backgroundColor', '#FF8DA1');
     })
+
+
 });
